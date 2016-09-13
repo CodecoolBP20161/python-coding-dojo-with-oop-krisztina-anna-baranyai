f33a432f6db938221b972794d5ffd101e4c70be4
vbenedek <xbenedek01@gmail.com>
2016-09-13 09:58:49 +0200

    4 test ok

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
 
 class Contact:
-    pass
+
+    all_contacts = ContactList()
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        Contact.all_contacts.append(self)
+
+    @classmethod
+    def reset_contacts(cls):
+        cls.all_contacts = []
+
+
+class Supplier(Contact):
+    all_orders = {}
+
+    def __init__(self, name, email):
+        super().__init__(name, email)
+
+    def order(self, string):
+        Supplier.all_orders[self.name] = string
+
+
+class ContactList(list):
+
+    def search(self, string):
+        pass
